2bdca3812c94c5713371136eb583a34e7b42299a
Ryan Beiden <rdbeiden@gmail.com>
2020-10-17 11:35:00 -0500

    add shift instance to walking animals

--- goat/request.py
+++ goat/request.py
@@ -1,5 +1,5 @@
 from models.goat import Goat
 
 def create_goat():
-    lawrence = Goat("Lawrence", "Fainting")
-    return print(f'| {lawrence.name} the {lawrence.species} goat is walking on {lawrence.date_added} |')
+    lawrence = Goat("Lawrence", "Fainting", "midday")
+    return print(f'| {lawrence.name} the {lawrence.species} goat is available to pet during the {lawrence.shift} shift. |')
--- lion/request.py
+++ lion/request.py
@@ -1,5 +1,5 @@
 from models.lion import Lion
 
 def create_lion():
-    aslan = Lion("Aslan", "Huge")
-    return print(f'| {aslan.name} the {aslan.species} lion is walking on {aslan.date_added} |')
+    aslan = Lion("Aslan", "Huge", "afternoon")
+    return print(f'| {aslan.name} the {aslan.species} lion is available to pet during the {aslan.shift} shift. |')
